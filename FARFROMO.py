d804482db21aa8cae7359139d666287c3bdac8ad
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-01-16 09:52:47 +0530

    Add files via upload

---
+++ FARFROMO.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for t in range(int(input())):
+    x1,y1,x2,y2=map(int,input().split())
+    d1=((x1**2) + (y1**2))
+    d2=((x2**2) + (y2**2))
+    
+    if(d1==d2):
+        print("EQUAL")
+    elif(d1>d2):
+        print('ALEX')
+    else:
+        print("BOB")
