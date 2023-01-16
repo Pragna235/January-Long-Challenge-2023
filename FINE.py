2db3faedbcec58dfd3b70d7b22876b0c13f6be15
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-01-16 09:48:26 +0530

    Add files via upload
    January 2023 Non-scorable Problem Division 3

---
+++ FINE.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for t in range(int(input())):
+    x=int(input())
+    if(x<=70):
+        print(0)
+    elif(x>70 and x<=100):
+        print(500)
+    else:
+        print(2000)
